4312fba70d9e57916b8f072bc0aad90de81987a3
Nour El Houda Guelmami <nourelhouda.guelmami@esprit.tn>
2023-11-05 14:17:27 +0100

    test JUnit commit

---
+++ src/test/java/tn/esprit/spring/services/CourseServiceTest.java
@@ -0,0 +1,32 @@
+package tn.esprit.spring.services;
+
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.entities.Course;
+import tn.esprit.spring.entities.Support;
+import tn.esprit.spring.entities.TypeCourse;
+
+import java.util.List;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class CourseServiceTest {
+    @Autowired
+    ICourseServices courseServices;
+
+    @Test
+    @Order(1)
+    public void testerRetriveAllCourses(){
+        List<Course> courseList = courseServices.retrieveAllCourses();
+        Assertions.assertEquals(0,courseList.size());
+    }
+
+    @Test
+    @Order(2)
+    public void testerSaveCourse(){
+        Course addedCourseTest = new Course(1L,2, TypeCourse.INDIVIDUAL, Support.SNOWBOARD,15.2f,2);
+        Course courseAdded = courseServices.addCourse(addedCourseTest);
+        Assertions.assertEquals(courseAdded.getNumCourse(),addedCourseTest.getNumCourse());
+    }
+}
